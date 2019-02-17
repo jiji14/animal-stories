bb1322c0bbe7ab5d5613430e3db685f8c9d2b59d
jiji14 <47590587+jiji14@users.noreply.github.com>
2019-02-17 10:45:51 +0900

    Update getImportantAnimals.py

--- getImportantAnimals.py
+++ getImportantAnimals.py
@@ -2,15 +2,15 @@
 
 def importantAnimals(keepAnimals_dic):
     if len(keepAnimals_dic) <= 2:
-        animal_ls =list(keepAnimals_dic.keys())
-        return animal_ls
+        animal_list =list(keepAnimals_dic.keys())
+        return animal_list
     else:   
-        animal_ls = list()
+        animal_list = list()
         for animals in range(2):
             maxdata = max(keepAnimals_dic, key=keepAnimals_dic.get)
             keepAnimals_dic.pop(maxdata)
-            animal_ls.append(maxdata)
-        return animal_ls
+            animal_list.append(maxdata)
+        return animal_list
 
             
 print (importantAnimals({"tortoise":2, "rabbit": 5, "goat": 7}))
